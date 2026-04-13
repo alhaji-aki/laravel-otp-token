407b39a3040821051260083e960acc3a9e058746
Abdul Kudus Issah <abdulkudus2922@gmail.com>
2026-04-13 06:30:16 +0000

    use pest for testing

--- tests/OtpTokenBrokerTest.php
+++ tests/OtpTokenBrokerTest.php
@@ -1,241 +1,198 @@
 <?php
 
-namespace AlhajiAki\OtpToken\Tests\Unit;
-
 use AlhajiAki\OtpToken\Contracts\CanSendOtpToken;
 use AlhajiAki\OtpToken\Contracts\OtpTokenBroker as OtpTokenBrokerContract;
 use AlhajiAki\OtpToken\OtpTokenBroker;
-use AlhajiAki\OtpToken\Tests\TestCase;
 use AlhajiAki\OtpToken\TokenRepositoryInterface;
 use Illuminate\Contracts\Auth\UserProvider;
 use Illuminate\Support\Arr;
 use Mockery as m;
-// use PHPUnit\Framework\TestCase;
 use UnexpectedValueException;
 
-class OtpTokenBrokerTest extends TestCase
+afterEach(function () {
+    m::close();
+});
+
+function getMocks(): array
 {
-    protected function tearDown(): void
-    {
-        m::close();
-    }
-
-    /** @test */
-    public function user_is_not_found_error_is_returned()
-    {
-        $mocks = $this->getMocks();
-
-        $broker = $this->getMockBuilder(OtpTokenBroker::class)
-            ->onlyMethods(['getUser'])
-            ->setConstructorArgs(array_values($mocks))
-            ->getMock();
-
-        $broker->expects($this->once())->method('getUser')->willReturn(null);
-
-        $this->assertSame(
-            OtpTokenBrokerContract::INVALID_USER,
-            $broker->sendOtpToken(['credentials', 'action' => 'action'], function () {
-                //
-            })
-        );
-    }
-
-    /** @test */
-    public function token_is_recently_created_error_is_returned()
-    {
-        $mocks = $this->getMocks();
-
-        $broker = $this->getMockBuilder(OtpTokenBroker::class)
-            ->addMethods(['sendToken'])
-            ->setConstructorArgs(array_values($mocks))
-            ->getMock();
-
-        $mocks['users']->shouldReceive('retrieveByCredentials')
-            ->once()
-            ->with(['foo'])
-            ->andReturn($user = m::mock(CanSendOtpToken::class));
-
-        $mocks['tokens']->shouldReceive('recentlyCreatedToken')
-            ->once()
-            ->with($user, 'action', 'field')
-            ->andReturn(true);
-
-        $user->shouldReceive('sendOtpTokenNotification')->with('token');
-
-        $this->assertSame(
-            OtpTokenBrokerContract::OTP_TOKEN_THROTTLED,
-            $broker->sendOtpToken(['foo', 'action' => 'action', 'field' => 'field'], function () {
-                //
-            })
-        );
-    }
-
-    /** @test */
-    public function get_user_throws_exception_if_user_does_not_implement_can_send_otp_token()
-    {
-        $this->expectException(UnexpectedValueException::class);
-        $this->expectExceptionMessage('User must implement CanSendOtpToken interface.');
-
-        $broker = $this->getBroker($mocks = $this->getMocks());
-
-        $mocks['users']->shouldReceive('retrieveByCredentials')->once()->with(['foo'])->andReturn('bar');
-
-        $broker->getUser(['foo']);
-    }
-
-    /** @test */
-    public function user_is_retrieved_by_credentials()
-    {
-        $broker = $this->getBroker($mocks = $this->getMocks());
-
-        $mocks['users']->shouldReceive('retrieveByCredentials')->once()->with(['foo'])->andReturn($user = m::mock(CanSendOtpToken::class));
-
-        $this->assertEquals($user, $broker->getUser(['foo']));
-    }
-
-    /** @test */
-    public function broker_creates_token_without_error()
-    {
-        $mocks = $this->getMocks();
-
-        $broker = $this->getMockBuilder(OtpTokenBroker::class)
-            ->addMethods(['sendToken'])
-            ->setConstructorArgs(array_values($mocks))
-            ->getMock();
-
-        $mocks['users']->shouldReceive('retrieveByCredentials')
-            ->once()
-            ->with(['foo'])
-            ->andReturn($user = m::mock(CanSendOtpToken::class));
-
-        $mocks['tokens']->shouldReceive('recentlyCreatedToken')
-            ->once()
-            ->with($user, 'action', 'field')
-            ->andReturn(false);
-
-        $mocks['tokens']->shouldReceive('create')
-            ->once()
-            ->with($user, 'action', 'field')
-            ->andReturn('123456');
-
-        $this->assertSame(
-            OtpTokenBrokerContract::OTP_TOKEN_SENT,
-            $broker->sendOtpToken(['foo', 'action' => 'action', 'field' => 'field'], function () {
-                //
-            })
-        );
-    }
-
-    /** @test */
-    public function send_otp_token_executes_the_callback_passed_to_it()
-    {
-        $executed = false;
-
-        $closure = function () use (&$executed) {
-            $executed = true;
-        };
-
-        $mocks = $this->getMocks();
-
-        $broker = $this->getMockBuilder(OtpTokenBroker::class)
-            ->addMethods(['sendToken'])
-            ->setConstructorArgs(array_values($mocks))->getMock();
-
-        $mocks['users']->shouldReceive('retrieveByCredentials')
-            ->once()
-            ->with(['foo'])
-            ->andReturn($user = m::mock(CanSendOtpToken::class));
-
-        $mocks['tokens']->shouldReceive('recentlyCreatedToken')
-            ->once()
-            ->with($user, 'action', 'field')
-            ->andReturn(false);
-
-        $mocks['tokens']->shouldReceive('create')
-            ->once()
-            ->with($user, 'action', 'field')
-            ->andReturn('token');
-
-        $this->assertEquals(
-            OtpTokenBrokerContract::OTP_TOKEN_SENT,
-            $broker->sendOtpToken(['foo', 'action' => 'action', 'field' => 'field'], $closure)
-        );
-
-        $this->assertTrue($executed);
-    }
-
-    /** @test */
-    public function invalid_user_is_returned_if_credentials_are_wrong_when_performing_an_action()
-    {
-        $broker = $this->getBroker($mocks = $this->getMocks());
-
-        $mocks['users']->shouldReceive('retrieveByCredentials')->once()->with(['creds'])->andReturn(null);
-
-        $this->assertSame(OtpTokenBrokerContract::INVALID_USER, $broker->performAction(['creds'], function () {
-            //
-        }));
-    }
-
-    /** @test */
-    public function invalid_token_is_returned_if_token_does_not_exist_when_performing_an_action()
-    {
-        $creds = ['token' => 'token', 'action' => 'action', 'field' => 'field'];
-        $broker = $this->getBroker($mocks = $this->getMocks());
-
-        $mocks['users']->shouldReceive('retrieveByCredentials')
-            ->once()
-            ->with(Arr::except($creds, ['token', 'action', 'field']))
-            ->andReturn($user = m::mock(CanSendOtpToken::class));
-
-        $mocks['tokens']->shouldReceive('exists')
-            ->with($user, 'token', 'action', 'field')
-            ->andReturn(false);
-
-        $this->assertSame(OtpTokenBrokerContract::INVALID_TOKEN, $broker->performAction($creds, function () {
-            //
-        }));
-    }
-
-    /** @test */
-    public function performs_action_token_and_calls_callback()
-    {
-        unset($_SERVER['__otptoken.test']);
-        $broker = $this->getMockBuilder(OtpTokenBroker::class)
-            ->onlyMethods(['validateOtpToken'])
-            ->setConstructorArgs(array_values($mocks = $this->getMocks()))
-            ->getMock();
-
-        $broker->expects($this->once())
-            ->method('validateOtpToken')
-            ->willReturn($user = m::mock(CanSendOtpToken::class));
-
-        $mocks['tokens']->shouldReceive('delete')
-            ->once()
-            ->with($user, 'action', 'field');
-
-        $callback = function ($user) {
-            $_SERVER['__otptoken.test'] = compact('user');
-
-            return 'foo';
-        };
-
-        $this->assertSame(
-            OtpTokenBrokerContract::ACTION_COMPLETED,
-            $broker->performAction(['token' => 'token', 'action' => 'action', 'field' => 'field'], $callback)
-        );
-        $this->assertEquals(['user' => $user], $_SERVER['__otptoken.test']);
-    }
-
-    protected function getBroker($mocks)
-    {
-        return new OtpTokenBroker($mocks['tokens'], $mocks['users']);
-    }
-
-    protected function getMocks()
-    {
-        return [
-            'tokens' => m::mock(TokenRepositoryInterface::class),
-            'users' => m::mock(UserProvider::class),
-        ];
-    }
+    return [
+        'tokens' => m::mock(TokenRepositoryInterface::class),
+        'users' => m::mock(UserProvider::class),
+    ];
 }
+
+function getBroker(array $mocks): OtpTokenBroker
+{
+    return new OtpTokenBroker($mocks['tokens'], $mocks['users']);
+}
+
+it('user is not found error is returned', function () {
+    $mocks = getMocks();
+
+    $broker = $this->getMockBuilder(OtpTokenBroker::class)
+        ->onlyMethods(['getUser'])
+        ->setConstructorArgs(array_values($mocks))
+        ->getMock();
+
+    $broker->expects($this->once())->method('getUser')->willReturn(null);
+
+    expect(
+        $broker->sendOtpToken(['credentials', 'action' => 'action'], function () {
+            //
+        })
+    )->toBe(OtpTokenBrokerContract::INVALID_USER);
+});
+
+it('token is recently created error is returned', function () {
+    $mocks = getMocks();
+    $broker = getBroker($mocks);
+
+    $mocks['users']->shouldReceive('retrieveByCredentials')
+        ->once()
+        ->with(['foo'])
+        ->andReturn($user = m::mock(CanSendOtpToken::class));
+
+    $mocks['tokens']->shouldReceive('recentlyCreatedToken')
+        ->once()
+        ->with($user, 'action', 'field')
+        ->andReturn(true);
+
+    $user->shouldReceive('sendOtpTokenNotification')->with('token');
+
+    expect(
+        $broker->sendOtpToken(['foo', 'action' => 'action', 'field' => 'field'], function () {
+            //
+        })
+    )->toBe(OtpTokenBrokerContract::OTP_TOKEN_THROTTLED);
+});
+
+it('get user throws exception if user does not implement can send otp token', function () {
+    $broker = getBroker($mocks = getMocks());
+
+    $mocks['users']->shouldReceive('retrieveByCredentials')->once()->with(['foo'])->andReturn('bar');
+
+    $broker->getUser(['foo']);
+})->throws(UnexpectedValueException::class, 'User must implement CanSendOtpToken interface.');
+
+it('user is retrieved by credentials', function () {
+    $broker = getBroker($mocks = getMocks());
+
+    $mocks['users']->shouldReceive('retrieveByCredentials')->once()->with(['foo'])->andReturn($user = m::mock(CanSendOtpToken::class));
+
+    expect($broker->getUser(['foo']))->toBe($user);
+});
+
+it('broker creates token without error', function () {
+    $mocks = getMocks();
+    $broker = getBroker($mocks);
+
+    $mocks['users']->shouldReceive('retrieveByCredentials')
+        ->once()
+        ->with(['foo'])
+        ->andReturn($user = m::mock(CanSendOtpToken::class));
+
+    $mocks['tokens']->shouldReceive('recentlyCreatedToken')
+        ->once()
+        ->with($user, 'action', 'field')
+        ->andReturn(false);
+
+    $mocks['tokens']->shouldReceive('create')
+        ->once()
+        ->with($user, 'action', 'field')
+        ->andReturn('123456');
+
+    expect(
+        $broker->sendOtpToken(['foo', 'action' => 'action', 'field' => 'field'], function () {
+            //
+        })
+    )->toBe(OtpTokenBrokerContract::OTP_TOKEN_SENT);
+});
+
+it('send otp token executes the callback passed to it', function () {
+    $executed = false;
+
+    $closure = function () use (&$executed) {
+        $executed = true;
+    };
+
+    $mocks = getMocks();
+    $broker = getBroker($mocks);
+
+    $mocks['users']->shouldReceive('retrieveByCredentials')
+        ->once()
+        ->with(['foo'])
+        ->andReturn($user = m::mock(CanSendOtpToken::class));
+
+    $mocks['tokens']->shouldReceive('recentlyCreatedToken')
+        ->once()
+        ->with($user, 'action', 'field')
+        ->andReturn(false);
+
+    $mocks['tokens']->shouldReceive('create')
+        ->once()
+        ->with($user, 'action', 'field')
+        ->andReturn('token');
+
+    expect(
+        $broker->sendOtpToken(['foo', 'action' => 'action', 'field' => 'field'], $closure)
+    )->toBe(OtpTokenBrokerContract::OTP_TOKEN_SENT);
+
+    expect($executed)->toBeTrue();
+});
+
+it('invalid user is returned if credentials are wrong when performing an action', function () {
+    $broker = getBroker($mocks = getMocks());
+
+    $mocks['users']->shouldReceive('retrieveByCredentials')->once()->with(['creds'])->andReturn(null);
+
+    expect($broker->performAction(['creds'], function () {
+        //
+    }))->toBe(OtpTokenBrokerContract::INVALID_USER);
+});
+
+it('invalid token is returned if token does not exist when performing an action', function () {
+    $creds = ['token' => 'token', 'action' => 'action', 'field' => 'field'];
+    $broker = getBroker($mocks = getMocks());
+
+    $mocks['users']->shouldReceive('retrieveByCredentials')
+        ->once()
+        ->with(Arr::except($creds, ['token', 'action', 'field']))
+        ->andReturn($user = m::mock(CanSendOtpToken::class));
+
+    $mocks['tokens']->shouldReceive('exists')
+        ->with($user, 'token', 'action', 'field')
+        ->andReturn(false);
+
+    expect($broker->performAction($creds, function () {
+        //
+    }))->toBe(OtpTokenBrokerContract::INVALID_TOKEN);
+});
+
+it('performs action token and calls callback', function () {
+    unset($_SERVER['__otptoken.test']);
+
+    $broker = $this->getMockBuilder(OtpTokenBroker::class)
+        ->onlyMethods(['validateOtpToken'])
+        ->setConstructorArgs(array_values($mocks = getMocks()))
+        ->getMock();
+
+    $broker->expects($this->once())
+        ->method('validateOtpToken')
+        ->willReturn($user = m::mock(CanSendOtpToken::class));
+
+    $mocks['tokens']->shouldReceive('delete')
+        ->once()
+        ->with($user, 'action', 'field');
+
+    $callback = function ($user) {
+        $_SERVER['__otptoken.test'] = compact('user');
+
+        return 'foo';
+    };
+
+    expect(
+        $broker->performAction(['token' => 'token', 'action' => 'action', 'field' => 'field'], $callback)
+    )->toBe(OtpTokenBrokerContract::ACTION_COMPLETED);
+
+    expect($_SERVER['__otptoken.test'])->toBe(['user' => $user]);
+});
